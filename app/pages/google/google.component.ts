bfa3a8c3f2f29ab9ce4d6953802261c3d58e3d48
eesdil <kcs@aquantis.net>
2016-04-05 09:20:06 +0200

    Follwoing anuglar way helps...

--- app/pages/google/google.component.ts
+++ app/pages/google/google.component.ts
@@ -1,6 +1,4 @@
 import {Component, OnInit} from "angular2/core";
-import {Page} from "ui/page";
-import {topmost} from "ui/frame";
 import * as webViewModule from "ui/web-view";
 import {Config} from '../../shared/config';
 
@@ -11,30 +9,22 @@ import {Config} from '../../shared/config';
 export class GooglePage implements OnInit {
 
     url: string;
-    page: Page;
 
     constructor() {
     }
     ngOnInit() {
-        this.page = <Page>topmost().currentPage;
-    }
-    ngAfterContentInit() {
-        let webView = this.page.getViewById('myWebView');
-        // let webView = new webViewModule.WebView();
-        webView.on(webViewModule.WebView.loadFinishedEvent, (args) => {
-            console.log('load finished');
-            let message;
-            if (!args.error) {
-                message = "WebView finished loading " + args.url;
-            }
-            else {
-                message = "Error loading " + args.url + ": " + args.error;
-            }
-            console.log(message);
-        });
         this.url = Config.apiUrl + 'auth/google';
-        // not working with this...
-        // webView.src = this.url;
+    }
+    loaded(args) {
+        console.log('load finished');
+        let message;
+        if (!args.error) {
+            message = "WebView finished loading " + args.url;
+        }
+        else {
+            message = "Error loading " + args.url + ": " + args.error;
+        }
+        console.log(message);
     }
 }
 
--- app/shared/config.ts
+++ app/shared/config.ts
@@ -1,5 +1,5 @@
 export class Config {
-  static apiUrl = "http://172.19.146.37:3000/";
+  static apiUrl = "http://localhost:3000/";
   static token = "";
 }
 
